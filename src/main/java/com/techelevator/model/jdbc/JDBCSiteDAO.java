057b6e87bd0ca0c66ef03f23ad0856882efa9739
Archie Cooley <acooley011@gmail.com>
2020-03-05 18:20:09 -0500

    continueing

--- src/main/java/com/techelevator/model/jdbc/JDBCSiteDAO.java
+++ src/main/java/com/techelevator/model/jdbc/JDBCSiteDAO.java
@@ -1,11 +1,13 @@
 package com.techelevator.model.jdbc;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.sql.DataSource;
 
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.rowset.SqlRowSet;
 
 import com.techelevator.model.Site;
 import com.techelevator.model.SiteDAO;
@@ -21,8 +23,31 @@ public class JDBCSiteDAO implements SiteDAO {
 	@Override
 	public List<Site> searchForSiteInDateRange(Long selectedCampgroundID, LocalDate userToDate,
 			LocalDate userFromDate) {
-		// TODO Auto-generated method stub
-		return null;
+
+		List<Site> output = new ArrayList<Site>();
+		
+		String sqlSearchForSite = "SELECT site.* FROM site JOIN reservation ON site.site_id = reservation.site_id "
+				+ "WHERE ? NOT BETWEEN reservation.to_date AND reservation.from_date "
+				+ "AND ? NOT BETWEEN reservation.to_date AND reservation.from_date";
+		
+		SqlRowSet results = jdbcTemplate.queryForRowSet(sqlSearchForSite, userToDate, userFromDate);
+		
+		while(results.next()) {
+			Site nextSite = new Site();
+			nextSite.setSiteId(results.getLong("site_id"));
+			nextSite.setCampgroundId(results.getLong("campground_id"));
+			nextSite.setSiteNumber(results.getInt("site_number"));
+			nextSite.setMaxOccupancy(results.getInt("max_occupancy"));
+			nextSite.setAccessible(results.getBoolean("site_number"));
+			nextSite.setMaxRVLength(results.getInt("max_rv_length"));
+			nextSite.setSiteNumber(results.getInt("site_number"));
+			nextSite.setUtilities(results.getBoolean("utilities"));
+			
+			output.add(nextSite);
+		}
+		
+		return output;
+		
 	}
 
 }
