991414ff1997a11cef430a8241b162e3ad2bb629
Archie Cooley <acooley011@gmail.com>
2020-03-06 18:09:20 -0500

    keep going

--- src/test/java/com/techelevator/model/jdbc/JDBCReservationDAOTest.java
+++ src/test/java/com/techelevator/model/jdbc/JDBCReservationDAOTest.java
@@ -1,9 +1,94 @@
 package com.techelevator.model.jdbc;
 
-import com.techelevator.DAOIntegrationTest;
+import static org.junit.Assert.assertEquals;
 
+import java.sql.SQLException;
+import java.time.LocalDate;
+
+import javax.sql.DataSource;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runners.MethodSorters;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.SingleConnectionDataSource;
+
+import com.techelevator.DAOIntegrationTest;
+import com.techelevator.model.Park;
+import com.techelevator.model.Reservation;
+
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class JDBCReservationDAOTest extends DAOIntegrationTest{
 
+	private static SingleConnectionDataSource dataSource;
+	private JDBCReservationDAO dao;
+	JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
 	
+	private Reservation getReservation(long id, String name, long siteID, LocalDate createDate, LocalDate toDate, LocalDate fromDate) {
+		Reservation theReservation = new Reservation();
+		theReservation.setName(name);
+		theReservation.setReservationId(id);
+		theReservation.setSiteId(siteID);
+		theReservation.setCreateDate(createDate);
+		theReservation.setToDate(toDate);;
+		theReservation.setFromDate(fromDate);
+		return theReservation;
+	}
+	
+	private void assertReservationsAreEqual(Reservation expected, Reservation actual) {
+		assertEquals(expected.getReservationId(), actual.getReservationId());
+		assertEquals(expected.getName(), actual.getName());
+		assertEquals(expected.getSiteId(), actual.getSiteId());
+
+	}
+
+	/* Before any tests are run, this method initializes the datasource for testing. */
+	@BeforeClass
+	public static void setupDataSource() {
+		dataSource = new SingleConnectionDataSource();
+		dataSource.setUrl("jdbc:postgresql://localhost:5432/capstone-2");
+		dataSource.setUsername("postgres");
+		dataSource.setPassword("postgres1");
+		/* The following line disables autocommit for connections
+		 * returned by this DataSource. This allows us to rollback
+		 * any changes after each test */
+		dataSource.setAutoCommit(false);
+	}
+
+	/* After all tests have finished running, this method will close the DataSource */
+	@AfterClass
+	public static void closeDataSource() throws SQLException {
+		dataSource.destroy();
+	}
+
+	@Before
+	public void setup() {
+		
+		dao = new JDBCReservationDAO(dataSource);
+	}
+	
+	/* After each test, we rollback any changes that were made to the database so that
+	 * everything is clean for the next test */
+	@After
+	public void rollback() throws SQLException {
+		dataSource.getConnection().rollback();
+	}
+
+	/* This method provides access to the DataSource for subclasses so that
+	 * they can instantiate a DAO for testing */
+	protected DataSource getDataSource() {
+		return dataSource;
+	}
+	
+	@Test
+	public void createReservationFromSiteTest() {
+		
+		
+		
+	}
 	
 }
