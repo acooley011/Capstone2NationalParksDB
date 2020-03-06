ee93a90e189d46397587a08e558254d19c69abdf
Archie Cooley <acooley011@gmail.com>
2020-03-06 10:21:00 -0500

    reservation method work

--- src/main/java/com/techelevator/model/ReservationDAO.java
+++ src/main/java/com/techelevator/model/ReservationDAO.java
@@ -1,8 +1,10 @@
 package com.techelevator.model;
 
+import java.util.Date;
+
 public interface ReservationDAO {
 	
-	public void createReservationFromSite(int selectedSite, String name);
+	public void createReservationFromSite(int selectedSite, String name, Date userToDate, Date userFromDate);
 	//create (insert into) a reservation to selected site (site can only be selected if availble through 
 		//Site search method) with the given name. return reservation ID when created 
 
--- src/main/java/com/techelevator/model/jdbc/JDBCReservationDAO.java
+++ src/main/java/com/techelevator/model/jdbc/JDBCReservationDAO.java
@@ -1,12 +1,43 @@
 package com.techelevator.model.jdbc;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import javax.sql.DataSource;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.rowset.SqlRowSet;
+
+import com.techelevator.model.Reservation;
 import com.techelevator.model.ReservationDAO;
+import com.techelevator.model.Site;
 
 public class JDBCReservationDAO implements ReservationDAO {
+	
+	private JdbcTemplate jdbcTemplate;
+
+	public JDBCReservationDAO(DataSource dataSource) {
+		this.jdbcTemplate = new JdbcTemplate(dataSource);
+		
+	}
 
 	@Override
-	public void createReservationFromSite(int selectedSite, String name) {
-		// TODO Auto-generated method stub
+	public void createReservationFromSite(int selectedSite, String name, Date userToDate, Date userFromDate) {
+		
+		List<Reservation> output = new ArrayList<Reservation>();
+		
+		String sqlCreateReservation = "INSERT INTO reservation (SiteId, name, "
+				+ "fromDate, toDate, createDate) VALUES (?, ?, ?, ?) RETURNING reservation_id";
+		
+		SqlRowSet results = jdbcTemplate.queryForRowSet(sqlCreateReservation, selectedSite, name, userToDate, userFromDate);
+	
+		while(results.next()) {
+			Site nextReservation = new Site();
+			
+			
+		}
 		
 	}
 
